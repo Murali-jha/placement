4267a00f2a1a5243deaa233e1a11b1494941d314
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-04 20:27:15 +0530

    Create using hashing.cpp

---
+++ Linked List/Day-2/2. Linked List Cycle/using hashing.cpp
@@ -0,0 +1,9 @@
+bool cycleDetect(node* head) {
+    unordered_set<node*> hashTable;
+    while(head != NULL) {
+        if(hashTable.find(head) != hashTable.end()) return true;
+        hashTable.insert(head);
+        head = head->next;
+    }
+    return false;
+}
