bb6bea651f8ed32a8cf49c14396e5dcbd7675887
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-10 18:47:04 +0530

    Create solution.cpp

---
+++ Binary Search/SDE/3. Single Element in a Sorted Array/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low =  0;
+        int mid ;
+        int high  = nums.size()-1;
+        
+        
+        if(high == 0){
+            return nums[0];
+        }
+        else if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        else if(nums[high]!=nums[high-1]){
+            return nums[high];
+        }
+        
+        
+        while(low<=high){
+            mid = low + (high-low)/2;
+            
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1])
+                return nums[mid];
+            else if(((mid%2)==0 && nums[mid+1]==nums[mid]) || ((mid%2)!=0 && nums[mid]==nums[mid-1])){
+                low = mid+1;
+            }
+            else
+                high = mid-1;
+        }
+        return 0;
+    }
+};
