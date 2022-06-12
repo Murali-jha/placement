0485e76a258ae6b44e80e51da9e04b2f95fc1890
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-12 17:13:48 +0530

    Create optimal.cpp

---
+++ Stack & Queues/Playlist/4. Nearest smaller to right/optimal.cpp
@@ -0,0 +1,15 @@
+vector<long long> nextLargerElement(vector<long long> arr, int n){
+        // Your code here
+        vector<long long> ans(n);
+        int j = n-1;
+        stack<long long> s;
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() && s.top()>=arr[i]){
+                s.pop();
+            }
+            long long x = s.empty()?-1:s.top();
+            ans[j--] = (x);
+            s.push(arr[i]);
+        }
+        return ans;
+    }
