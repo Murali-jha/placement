39003d3eb5b89aee5027ea69f39dbeaac2a0919c
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-15 22:48:26 +0530

    Create brute.cpp

---
+++ String/Day-1/5. Longest Common Prefix/brute.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size()==1){
+            return strs[0];
+        }
+        else if(strs.size()==0){
+            return "";
+        }
+        
+        int i = 0;
+        string ans = "";
+        while(1){
+            int j = 1;
+            char target = strs[0][i];
+            while(j<strs.size()){
+                if(strs[j].length()==i || strs[j-1].length()==i){
+                    return ans;
+                }
+                if(strs[j-1][i]!=strs[j][i]){
+                    return ans;
+                }
+                
+                j++;
+            }
+            
+            ans += target;
+            i++;
+        }
+        return ans;
+    }
+};
