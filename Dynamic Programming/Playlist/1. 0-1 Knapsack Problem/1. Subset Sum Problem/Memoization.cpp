aed6f4b47d3da5378a306fd9f7b592e0390d328a
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-17 08:41:52 +0530

    Create Memoization.cpp

---
+++ Dynamic Programming/Playlist/1. 0-1 Knapsack Problem/1. Subset Sum Problem/Memoization.cpp
@@ -0,0 +1,27 @@
+bool solve(vector<int>arr, int sum, int n,vector<vector<int>> &t){
+        if(sum==0){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        
+        if(t[n][sum]!=-1){
+            return t[n][sum];
+        }
+        
+        if(arr[n-1]<=sum){
+            return t[n][sum] = solve(arr,sum-arr[n-1],n-1,t) || solve(arr,sum,n-1,t);
+        }
+        else{
+            return t[n][sum] = solve(arr,sum,n-1,t);
+        }
+    }
+
+
+    bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n = arr.size();
+        vector<vector<int>> t(n+1,vector<int>(sum+1,-1));
+        return solve(arr,sum,n,t);
+    }
