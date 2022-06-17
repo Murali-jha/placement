69f957e1cee0fd84ebb26bf0d6c390964e608a89
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-17 09:47:31 +0530

    Create Tabulation.cpp

---
+++ Dynamic Programming/Playlist/1. 0-1 Knapsack Problem/3. Count of Subsets with the given sum/Tabulation.cpp
@@ -0,0 +1,24 @@
+bool isSubsetSum(vector<int>arr, int sum){
+        // code here 
+        int n = arr.size();
+        vector<vector<int>> t(n+1,vector<int>(sum+1,-1));
+        
+        for(int i=1;i<=sum;i++){
+            t[0][i] = 0;
+        }
+        for(int i=0;i<=n;i++){
+            t[i][0] = 1;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(arr[i-1]<=j){
+                    t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j];
+                }
+                else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+    }
