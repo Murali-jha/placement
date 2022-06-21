1fc26499aef53c9723a521b30f7ca7c7e362ac46
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-21 10:28:51 +0530

    Create TLE.cpp

---
+++ Dynamic Programming/SDE/Day-2/4. Minimum Cost to Cut a Stick/TLE.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    
+    
+    int solve(vector<int> &cuts, int i,int j,vector<vector<int>> &t){
+        if(i>j){
+            return 0;
+        }    
+        
+        if(t[i][j]!=-1)
+            return t[i][j];
+        
+        int ans = INT_MAX;
+        for(int k=i;k<=j;k++){
+            int temp = solve(cuts,i,k-1,t) + solve(cuts,k+1,j,t) + (cuts[j+1]-cuts[i-1]);
+            
+            ans = min(ans,temp);
+        }
+        
+        return t[i][j] = ans;
+    }
+    
+    
+    
+    
+    int minCost(int n, vector<int>& cuts) {
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(),cuts.end());
+        vector<vector<int>> t(n+1,vector<int>(n+1,-1));
+        return solve(cuts,1,cuts.size()-2,t);
+    }
+};
