b45cfd48a16afa050e26c6ae64c17ed2b2a8aa3d
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-23 23:58:55 +0530

    Create iterative(using stack).cpp

---
+++ Trees/Day-3/5. Flatten Binary Tree/iterative(using stack).cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        stack<TreeNode*> st;
+        if(!root)
+            return;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* temp  = st.top();
+            st.pop();
+            
+            if(temp->right){
+                st.push(temp->right);
+            }
+            if(temp->left){
+                st.push(temp->left);
+            }
+            
+            
+            if(!st.empty()){
+                temp->right = st.top();
+            }
+            temp->left = NULL;
+        }
+    }
+};
