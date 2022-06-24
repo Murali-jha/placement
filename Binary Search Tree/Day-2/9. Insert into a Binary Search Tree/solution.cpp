ff25ca5c45e2e36cef44a41cefa837929f6b97f2
Murali Jha <70423437+Murali-jha@users.noreply.github.com>
2022-06-24 20:52:31 +0530

    Create solution.cpp

---
+++ Binary Search Tree/Day-2/9. Insert into a Binary Search Tree/solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==NULL){
+            return new TreeNode(val);
+        }
+        
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->val>val){
+                if(curr->left==NULL){
+                    curr->left = new TreeNode(val);
+                    return root;
+                }
+                else{
+                    curr = curr->left;
+                }
+            }
+            else{
+                if(curr->right==NULL){
+                    curr->right = new TreeNode(val);
+                    return root;
+                }
+                else{
+                    curr = curr->right;
+                }
+            }
+        }
+        return root;
+    }
+};
